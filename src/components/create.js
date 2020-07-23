a938c9b2f396fe0ae5c48d8595d4bfafcfd2be2b
v_xiongzicheng <v_xiongzicheng@baidu.com>
2020-07-23 18:12:41 +0800

    fix

--- src/components/create.js
+++ src/components/create.js
@@ -1,24 +1,24 @@
 import Vue from 'vue';
-// function create(component, props) {
-//   // render函数将组件配置对象转化成虚拟dom
-//   const vm = new Vue({render: h => h(component, {props})}).$mount();
-//   document.body.appendChild(vm.$el);
-//   const comp = vm.$children[0];
-//   comp.remove = () => {
-//     document.body.removeChild(vm.$el);
-//     vm.$destroy();
-//   };
-//   return comp;
-// }
-
-export default create;
-
-function createRe(component, props) {
+function create(component, props) {
+  // render函数将组件配置对象转化成虚拟dom
   const vm = new Vue({render: h => h(component, {props})}).$mount();
   document.body.appendChild(vm.$el);
   const comp = vm.$children[0];
   comp.remove = () => {
+    document.body.removeChild(vm.$el);
     vm.$destroy();
   };
   return comp;
 }
+
+export default create;
+
+// function createRe(component, props) {
+//   const vm = new Vue({render: h => h(component, {props})}).$mount();
+//   document.body.appendChild(vm.$el);
+//   const comp = vm.$children[0];
+//   comp.remove = () => {
+//     vm.$destroy();
+//   };
+//   return comp;
+// }
